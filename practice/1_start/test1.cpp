9765b24548858571995373b0ae4a43dd60f69cfd
pranav083 <kpranav083@gmail.com>
2020-12-09 00:52:31 +0530

    file code updated : test1.cpp

---
+++ practice/1_start/test1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+#define OJ \
+    (void)!freopen("../../../input.txt", "r", stdin); \
+    (void)!freopen("../../../error.txt", "w", stderr); \
+    (void)!freopen("../../../output.txt", "w", stdout);
+
+int main() 
+{ OJ
+	 // Write your code here
+	return 0; 
+} 
