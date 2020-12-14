fa3f9cc9497009a5a5a75a3f9e7c4bf1557b102a
pranav083 <kpranav083@gmail.com>
2020-12-15 02:16:29 +0530

    file code updated : struct_c.cpp

---
+++ practice/udemy_dsa/2_basis/struct_c.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define OJ \
+    (void)!freopen("../../../input.txt", "r", stdin); \
+    (void)!freopen("../../../error.txt", "w", stderr); \
+    (void)!freopen("../../../output.txt", "w", stdout);
+
+struct Rectangle
+{
+	int length;
+	int breath;
+};
+
+void intilize_length(struct Rectangle *r, int l) {
+	r -> length = l;
+}
+void intilize_breath(struct Rectangle *r, int b) {
+	r -> breath = b;
+}
+int area(struct Rectangle r) {
+	return r.length * r.breath;
+}
+void change_length(struct Rectangle *r, int l) {
+	r -> length = l;
+}
+void change_breath(struct Rectangle *r, int b) {
+	r -> breath = b;
+}
+int main()
+{	OJ
+	struct Rectangle r;
+	int l , b;
+	cin >> l >> b;
+	intilize_length(&r, l);
+	intilize_breath(&r, b);
+	cout << area(r) << endl;
+	cin >> l;
+	change_length(&r, l);
+	cout << area(r) << endl;
+
+
+	return 0;
+}
