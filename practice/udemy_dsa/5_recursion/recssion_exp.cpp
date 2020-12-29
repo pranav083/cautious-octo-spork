cf008319430d9ca53e11df23ad8bf2d5d04fed32
pranav083 <kpranav083@gmail.com>
2020-12-30 00:38:27 +0530

    file code updated : recssion_exp.cpp

---
+++ practice/udemy_dsa/5_recursion/recssion_exp.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef OJ
+#define OJ \
+    (void)!freopen("../../../input.txt", "r", stdin); \
+    (void)!freopen("../../../error.txt", "w", stderr); \
+    (void)!freopen("../../../output.txt", "w", stdout);
+#endif
+
+int expo(int a, int n)
+{
+	if (n == 0)
+	{
+		return 1;
+	}
+	else if (n % 2 == 0)
+	{
+		return expo(a * a, n / 2);
+	}
+	else
+	{
+		return a * expo(a * a, (n - 1) / 2);
+	}
+}
+
+int main()
+{	OJ
+	int a, b, c;
+	cin >> a >> b;
+	c = expo(a, b);
+	cout << c << endl;
+}
