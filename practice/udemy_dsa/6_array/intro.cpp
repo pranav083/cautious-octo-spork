766eb57a487b167cc9b3d4019efe3e68ffb60f5d
pranav083 <kpranav083@gmail.com>
2021-01-05 22:32:52 +0530

    file code updated : intro.cpp
    Signed-off-by: pranav083 <kpranav083@gmail.com>

--- practice/udemy_dsa/6_array/intro.cpp
+++ practice/udemy_dsa/6_array/intro.cpp
@@ -11,7 +11,19 @@ using namespace std;
 #endif
 int main()
 {   OJ;
-	int a ;
+	int b[3][2]= {{3,4},{3,3},{2,6}} ;
+	int a;
+	
+	int *c[3];
+	c[0] = new int[2]; 
+	c[1] = new int[2];
+	c[2] = new int[2];
+	
+	int **d;
+	d = new int*[3];
+	d[0] = new int[2];
+	d[1] = new int[2];
+	d[2] = new int[2];
 	cin>>a;
 	cout<<a;
 
