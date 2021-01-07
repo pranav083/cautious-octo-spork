1805aff50cd2c6ab286c43e48eb5cf1eef962b9a
pranav083 <kpranav083@gmail.com>
2021-01-07 23:52:06 +0530

    task added

--- practice/1_start/a.cpp
+++ practice/1_start/a.cpp
@@ -2,37 +2,18 @@
 #include <iostream>
 using namespace std;
 
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-#define endl "\n"
-#define sd(val) scanf("%d", &val)
-#define ss(val) scanf("%s", &val)
-#define sl(val) scanf("%lld", &val)
-#define debug(val) printf("check%d\n", val)
-#define all(v) v.begin(), v.end()
-#define PB push_back
-#define MP make_pair
-#define FF first
-#define SS second
-#define ll long long
-#define MOD 1000000007
-#define clr(val) memset(val, 0, sizeof(val))
-#define what_is(x) cerr << #x << " is " << x << endl;
-#define OJ                            \
-    freopen("input.txt", "r", stdin); \
-    freopen("output.txt", "w", stdout);
-#define FIO                           \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL);
-
+#ifndef OJ
+#define OJ \
+    (void)!freopen("../../input.txt", "r", stdin); \
+    (void)!freopen("../../error.txt", "w", stderr); \
+    (void)!freopen("../../output.txt", "w", stdout);
+#endif
 int main()
 {
     OJ;
     int i;
     cin >> i;
-    cout << "hello world ";
+    cout << "hello   world ";
     cout << i * i * i;
-
     return 0;
 }
--- practice/udemy_dsa/6_array/array_declaration.cpp
+++ practice/udemy_dsa/6_array/array_declaration.cpp
@@ -1,11 +1,11 @@
 // this code is about how to define an array in program
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 #ifndef OJ
-#define OJ \
-    (void)!freopen("../../../input.txt", "r", stdin); \
+#define OJ                                             \
+    (void)!freopen("../../../input.txt", "r", stdin);  \
     (void)!freopen("../../../error.txt", "w", stderr); \
     (void)!freopen("../../../output.txt", "w", stdout);
 #endif
@@ -16,20 +16,20 @@ struct Array
     int length;
 };
 
-void append (struct Array *arry , int x)
+void append(struct Array *arry, int x)
 {
-    if(arry->length < arry->size)
+    if (arry->length < arry->size)
     {
         arry->a[arry->length++] = x;
     }
-} 
+}
 
 void insert(struct Array *arry, int index, int value)
 {
-    if(arry->size >= index && index >= 0 )
+    if (arry->size >= index && index >= 0)
     {
-        for (int i = arry->length ; i<index; i--)
-            arry->a[i] = arry->a[i-1];
+        for (int i = arry->length; i < index; i--)
+            arry->a[i] = arry->a[i - 1];
         arry->a[index] = value;
         arry->length++;
     }
@@ -38,35 +38,35 @@ void entry_data(struct Array *arry)
 {
     for (int i = 0; i < arry->size; i++)
     {
-        cin>>arry->a[i];
-        cout<<arry->a[i]<<endl;
+        cin >> arry->a[i];
         arry->length++;
     }
 }
 void delete_data(struct Array *arry, int index)
 {
-    if (index>=0 && index <= arry->length)
+    if (index >= 0 && index <= arry->length)
     {
-        for(int i= index; i< arry->length; i++) // as only the first line of the loop will be executed
+        for (int i = index; i < arry->length; i++) // as only the first line of the loop will be executed
             arry->a[i] = arry->a[i + 1];
         arry->length--;
     }
 }
 void display_arry(struct Array arry)
 {
-    for(int i = 0; i< arry.size; i++)
-        cout<<arry.a[i]<<endl;
+    for (int i = 0; i < arry.size; i++)
+        cout << arry.a[i] << endl;
 }
 
 int main()
-{   OJ;
+{
+    OJ;
     struct Array arry;
-    cout<<"enter the size of the array";
-    cin>>arry.size;
+    cout << "enter the size of the array";
+    cin >> arry.size;
     arry.a = new int[arry.size];
     arry.length = 0;
     entry_data(&arry);
-    insert(&arry, 3,299);
-    delete_data(&arry, 2);    
+    insert(&arry, 3, 299);
+    delete_data(&arry, 2);
     display_arry(arry);
 }
